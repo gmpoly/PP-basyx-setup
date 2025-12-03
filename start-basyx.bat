06678b2776e275cfba573c0bb8199ce57dfef2c3
GEM <georg.moebert@polyscale.com>
2025-12-03 08:41:35 +0100

    start und stop

---
+++ start-basyx.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Starte BaSyx Umgebung...
+cd /d "C:\Users\GEM\0 - PP\basyx-setup"
+docker compose up -d
+echo BaSyx gestartet!
+pause
---
+++ stop-basyx.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Stoppe BaSyx Umgebung...
+cd /d "C:\Users\GEM\0 - PP\basyx-setup"
+docker compose down
+echo BaSyx gestoppt!
+pause
